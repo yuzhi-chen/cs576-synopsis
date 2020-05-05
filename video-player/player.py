e15c624d9b8ed376e168d253bfe3802fda641dfc
shunkaiz <shunkaiz@usc.edu>
2020-05-05 03:16:06 -0700

    Enabled synopsis click to play

--- video-player/player.py
+++ video-player/player.py
@@ -4,16 +4,21 @@ import sys
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtGui import QIcon, QPalette, QPixmap
-from PyQt5.QtCore import Qt, QUrl, pyqtSignal
+from PyQt5.QtCore import Qt, QUrl, pyqtSignal, QSignalMapper
 import os
 import subprocess
+from pathlib import Path
+
+
+dirname = os.path.dirname(__file__)
+parent_dir = Path(__file__).parent.parent
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("PyQt5 Media Player")
-        self.setGeometry(0, 0, 700, 500)
+        self.setGeometry(0, 0, 700, 700)
         self.setWindowIcon(QIcon('player.png'))
 
         p = self.palette()
@@ -69,14 +74,16 @@ class Window(QWidget):
         hboxLayout.addWidget(self.slider)
 
         synopImageLayout = QHBoxLayout()
-        synopImageLayout.setContentsMargins(1, 1, 1, 1)
-        image1 = ClickLabel()
-        pixmap = QPixmap('../test.jpg')
-        image1.setPixmap(pixmap)
-        image1.setFixedSize(100, 50)
+        synopImageLayout.setContentsMargins(1, 0.5, 1, 0.5)
 
-        image1.clicked.connect(self.synopsis_click_handler)
-        synopImageLayout.addWidget(image1)
+        self.generate_synopsis(synopImageLayout)
+        # image1 = ClickLabel()
+        # pixmap = QPixmap('../test.jpg')
+        # image1.setPixmap(pixmap)
+        # image1.setFixedSize(100, 50)
+        #
+        # image1.clicked.connect(self.synopsis_click_handler)
+        # synopImageLayout.addWidget(image1)
 
 
         #create vbox layout
@@ -96,6 +103,24 @@ class Window(QWidget):
         self.mediaPlayer.stateChanged.connect(self.mediastate_changed)
         self.mediaPlayer.positionChanged.connect(self.position_changed)
         self.mediaPlayer.durationChanged.connect(self.duration_changed)
+
+    def generate_synopsis(self, synopImageLayout):
+        frame_folder = os.path.join(parent_dir, 'keyframes')
+        image_dir = os.listdir(frame_folder)
+        image_dir.remove('.gitignore')
+        image_dir.sort(key=lambda img: (int(img.split('_')[1].split('.')[0])))
+        images = [image for image in image_dir]
+        for imageName in images:
+            print(imageName)
+            # frame_time = int(imageName.split('_')[1].split('.')[0])
+            timestamp = (int(imageName.split('_')[1].split('.')[0]))
+            image_label = ClickLabel()
+            pixmap = QPixmap(os.path.join(parent_dir, 'keyframes', imageName))\
+                .scaled(100, 70, Qt.KeepAspectRatio, Qt.FastTransformation)
+            image_label.setPixmap(pixmap)
+            image_label.clicked.connect(lambda t=timestamp: self.synopsis_click_handler(t))
+            synopImageLayout.addWidget(image_label)
+
 
 
     def open_file(self):
@@ -125,16 +150,15 @@ class Window(QWidget):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.playBtn.setIcon(
                 self.style().standardIcon(QStyle.SP_MediaPause)
-
             )
 
         else:
             self.playBtn.setIcon(
                 self.style().standardIcon(QStyle.SP_MediaPlay)
-
             )
 
     def position_changed(self, position):
+        print(position)
         self.slider.setValue(position)
 
 
@@ -150,8 +174,10 @@ class Window(QWidget):
         self.playBtn.setEnabled(False)
         self.label.setText("Error: " + self.mediaPlayer.errorString())
 
-    def synopsis_click_handler(self):
-        self.mediaPlayer.setPosition(0)
+    def synopsis_click_handler(self, frame):
+        time = frame/30*1000
+        self.slider.setValue(time)
+        self.mediaPlayer.setPosition(time)
 
 
 def convert_avi_to_mp4(input_file, output_name):
@@ -162,7 +188,6 @@ def convert_avi_to_mp4(input_file, output_name):
 
 class ClickLabel(QLabel):
     clicked = pyqtSignal()
-
     def mousePressEvent(self, event):
         self.clicked.emit()
         QLabel.mousePressEvent(self, event)
